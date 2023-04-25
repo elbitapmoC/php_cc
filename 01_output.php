a3f787651c36d0859563511ec5508a82bf347e66
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-04-25 00:32:45 -0400

    1st commit

---
+++ 01_output.php
@@ -0,0 +1,35 @@
+<?php
+
+/* Multi-line Comment
+Another Comment
+End Comment */
+
+// echo - Output strings, numbers, html, etc.
+// echo('🤠 Hoooowdy!');
+
+// print - similar to echo, can accept 1 arg.
+// print('🐄');
+
+// print_r() - Print single values and arrays.
+// print_r(['🐔','🦆','🐈']);
+
+// var_dump() - Similar to print_r, gives us info like data type, length, etc. 
+// var_dump(['🐔','🦆','🐈'])
+
+// var_export() - outputs a string representation of a variable.
+// var_export('LLLL')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- Shortcut for echo -->
+  <h1><?= 'PLACE';?></h1>
+</body>
+</html>
